bf7a93f38c67353ef029f47149ad8da3b46ba73b
wolfcoder <bbg2021@gmail.com>
2016-02-14 13:22:37 +0800

    Create toped.js

---
+++ toped.js
@@ -0,0 +1,12 @@
+var list_jualan = "https://www.tokopedia.com/cozymart/kacamata-locs-original";
+
+var x = document.getElementsById("dink-it");
+ 
+var consolePush = function(){console.log("consolePush")};
+
+if (list_jualan == location.href){
+setInterval( function(){ 
+    consolePush() , x() }, 2 * 1000);}
+	
+	
+	
