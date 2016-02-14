c102d8a744a234f52d43ef679c043182a26b02f9
wolfcoder <bbg2021@gmail.com>
2016-02-14 13:30:04 +0800

    Update toped.js

--- toped.js
+++ toped.js
@@ -1,4 +1,4 @@
-var list_jualan = "https://www.tokopedia.com/cozymart/kacamata-locs-original";
+var list_jualan = "https://www.tokopedia.com/cozymart/kacamata-locs-original/";
 
 var x = document.getElementById("dink-it");
  
@@ -7,6 +7,4 @@ var consolePush = function(){console.log("consolePush")};
 if (list_jualan == location.href){
 setInterval( function(){ 
     consolePush() , x() }, 2 * 1000);}
-	
-	
 	
