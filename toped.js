1e9706169ebe42811857dc4153575c1f49c7aeec
wolfcoder <bbg2021@gmail.com>
2016-02-14 14:06:49 +0800

    Update toped.js

--- toped.js
+++ toped.js
@@ -1,10 +1,14 @@
 var list_jualan = "https://www.tokopedia.com/cozymart/kacamata-locs-original/";
 
 var x = document.getElementById("dink-it");
- 
+var clickPush = function(){x.click();}
 var consolePush = function(){console.log("consolePush")};
+
+var Popup = document.getElementsByClassName("dialog-close jqmClose");
+var closePopup = function(){Popup[0].click();};
+
+if (Popup != null){closePopup()}
 
 if (list_jualan == location.href){
 setInterval( function(){ 
-    consolePush() , x() }, 2 * 1000);}
-	
+    consolePush() , clickPush() }, 3600 * 1000);}
