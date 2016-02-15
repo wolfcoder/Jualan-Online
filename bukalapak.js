fa8bd315ab7466b16300b877431251cbb32a43e5
wolfcoder <bbg2021@gmail.com>
2016-02-15 11:55:00 +0800

    Update bukalapak.js
    tambah start

--- bukalapak.js
+++ bukalapak.js
@@ -1,12 +1,23 @@
-var list_jualan = "https://www.bukalapak.com/my_products?from=dropdown&page=1";
+// Here You can type your custom JavaScript input...
+var list_jualan = "https://www.bukalapak.com/my_products?from=dropdown";
 
 var x = document.getElementsByClassName("btn btn--orange btn--medium btn--block");
-var clickPush = function(){x[1].click();}
-var consolePush = function(){console.log("consolePush")};
+var clickPush = function(){x[0].click()};
+var clickPush1 = function(){x[1].click()};
+var clickPush2 = function(){x[2].click()};
+var clickPush3 = function(){x[3].click()};
+var date = new Date();
+ 
+console.log("Start: " + date );
+
+var consolePush = function(){console.log("consolePush 0")};
 
 if (list_jualan == location.href){
 setInterval( function(){ 
-    consolePush() , clickPush() }, 6000 * 1000);}
-	
-	
-	
+	var date = new Date();
+	var ms = date.getMilliseconds();
+    console.log(date + ms)  },60 * 1000);}
+    
+if (list_jualan == location.href){
+setInterval( function(){ 
+      clickPush() }, 601 * 1000);}
